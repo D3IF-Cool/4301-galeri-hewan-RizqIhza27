57de984b2c9f18897f9ea7b137315948cb6ee39d
RizqIhza27 <rihza27@gmail.com>
2021-05-03 19:06:31 +0700

    Memproses data internet sebagai JSON

--- app/src/main/java/org/d3if4001/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4001/galerihewan/MainActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
-import org.d3if4001.galerihewan.databinding.ActivityMainBinding
+
 import org.d3if4001.galerihewan.ui.main.MainAdapter
 
 class MainActivity : AppCompatActivity() {
--- app/src/main/java/org/d3if4001/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4001/galerihewan/ui/main/MainViewModel.kt
@@ -1,7 +1,33 @@
 package org.d3if4001.galerihewan.ui.main
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import org.d3if4001.galerihewan.model.Hewan
+import org.d3if4001.galerihewan.network.HewanApi
+import java.lang.Exception
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val data = MutableLiveData<List<Hewan>>()
+
+    init{
+        retrieveData()
+    }
+    private fun retrieveData(){
+        viewModelScope.launch {
+            try{
+
+                data.value = HewanApi.service.getHewan()
+            } catch (e : Exception){
+                Log.d("MainViewModel", "Failure: ${e.message}")
+            }
+        }
+    }
+
+
+    fun getData(): LiveData<List<Hewan>> = data
+
 }
